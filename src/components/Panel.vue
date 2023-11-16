fd03a24c9fd62d4d2c6f66ba105a07c679ad9efd
ifNil <ifnil.git@gmail.com>
2023-11-17 02:59:10 +0800

    update

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -29,18 +29,15 @@ onMounted(() => {
   <div
     class="grid h-screen grid-cols-[1fr_5px_3fr] grid-rows-[2fr_5px_1fr] gap-x-[0px] gap-y-[0px] bg-[#eef0f2]"
   >
-    <div class="rounded-[5px] bg-[#fdfdfe] [grid-area:1_/_1_/_4_/_2]"></div>
-    <div class="bg-[#eef0f2] [grid-area:1_/_2_/_4_/_3]" ref="div2"></div>
+    <div class="rounded-[5px] bg-[#fdfdfe] [grid-area:1/1/4/2]"></div>
+    <div class="bg-[#eef0f2] [grid-area:1/2/4/3]" ref="div2"></div>
     <div
-      class="mr-[calc(var(--move-interval)*(-1))] mb-[calc(var(--move-interval)*(-1))] rounded-[5px] bg-[#fdfdfe] [grid-area:1_/_3_/_2_/_4]"
+      class="mr-[calc(var(--move-interval)*(-1))] mb-[calc(var(--move-interval)*(-1))] rounded-[5px] bg-[#fdfdfe] [grid-area:1/3/2/4]"
     >
       <MonacoEditor />
     </div>
-    <div
-      class="col-[2/-1] bg-[#eef0f2] [grid-area:2_/_3_/_3_/_4]"
-      ref="div4"
-    ></div>
-    <div class="rounded-[5px] bg-[#fdfdfe] [grid-area:3_/_3_/_4_/_4]"></div>
+    <div class="col-[2/-1] bg-[#eef0f2] [grid-area:2/3/3/4]" ref="div4"></div>
+    <div class="rounded-[5px] bg-[#fdfdfe] [grid-area:3/3/4/4]"></div>
   </div>
 </template>
 
